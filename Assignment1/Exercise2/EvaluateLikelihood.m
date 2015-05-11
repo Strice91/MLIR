36789368d8c742331e932addc6eb51fb879f1972
Strice91 <r.strice@gmail.com>
2015-05-11 21:27:07 +0200

    EvaluateLikelihood.m

--- Assignment1/Exercise2/EvaluateLikelihood.m
+++ Assignment1/Exercise2/EvaluateLikelihood.m
@@ -5,8 +5,24 @@ img = double(imread(Image));
 H = size(img,1);
 W = size(img,2);
 
-rgb = reshape(img,[H*W,3]);
-mu = kron(mu,ones(1,size(rgb,2)));
+rgb = reshape(img,[3,H*W]);
+% mu = repmat(mu,1,size(rgb,2));
+% 
+% e = (Sigma \ (rgb-mu));
+
+p = zeros(1,H*W);
+
+S = Sigma^-1;
+D = det(Sigma);
+d = 3;
+
+for i = 1:H*W
+    e = -0.5 * (rgb(:,i)-mu)' * S * (rgb(:,i)-mu);
+    p(i) = exp(e) / ( (2*pi)^(d/2) * D^0.5 );
+end
+
+LikValues = reshape(p,H,W);
+
 
 end
 
