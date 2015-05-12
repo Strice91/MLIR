3132bd2d2b7fa15d3a956303aa16a4bbeccbb352
Strice91 <r.strice@gmail.com>
2015-05-12 22:23:16 +0200

    kmean

--- Assignment1/Exercise3/Exercise3_kmean.m
+++ Assignment1/Exercise3/Exercise3_kmean.m
@@ -13,23 +13,49 @@ n = size(gesture,2);
 % initital cluster posititons
 y = clusters;
 
-% adjacency matrix
+% Label matrix
 A = zeros(m,n);
 
 minimal_dist = ones(m,n) * inf;
 
+
+
+%% E-STEP:
+% Relate datapoints to clusters
 for k = 1:K
-   % Get distance of all Datapoints to current cluster center
-   all_dist = sqrt( (gesture(:,:,1)-y(k,1)).^2 +...
-                    (gesture(:,:,2)-y(k,2)).^2 +...
-                    (gesture(:,:,3)-y(k,3)).^2      );
-   % Indices of all Datapoints which have a smaller  
-   ind = all_dist <= minimal_dist;
-   A(ind) = k;
-   minimal_dist(ind) = all_dist(ind);
+   % Get distance of all datapoints to current k cluster-center
+   all_dist = sqrt( (gesture(:,:,1) - y(k,1)).^2 + ...
+                    (gesture(:,:,2) - y(k,2)).^2 + ...
+                    (gesture(:,:,3) - y(k,3)).^2      );
                 
+   % Indices of all datapoints which have a smaller 
+   % distance to current cluster
+   ind = all_dist <= minimal_dist;
+   % Write cluser label k to this indices
+   A(ind) = k;
+   % Update latest found minimal distance
+   minimal_dist(ind) = all_dist(ind);             
 end
 
+%% M-STEP
+% Update cluster centers. Set the cluster representative to the mean
+% of all datapoints in the culster
+for k = 1:K
+    % Find all datapoints labeld with k
+    ind = A==k;
+    % Clone Matrix for 3D indexing
+    ind3D = repmat(ind,1,1,3);
+    % Get all points of this cluster
+    points = gesture(ind3D);
+    % Reshape them in 3 component vectors
+    points = reshape(points,numel(points)/3,3);
+    % Get the mean of each spatial direktion
+    % for this cluster an update cluster representative
+    y(k,:) = mean(points);
+    % Get cluster distortion
+    cluster_dist = bsxfun(@minus,points,y(k,:));
+    cluster_dist = sum(sqrt(cluster_dist(:).^2));
+end
 
 
 end
