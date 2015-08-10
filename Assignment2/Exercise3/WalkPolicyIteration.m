19444287eb56159a872564e9e861d72da71158d5
Strice91 <r.strice@gmail.com>
2015-08-10 21:23:07 +0200

    Ex3 und tex

--- Assignment2/Exercise3/WalkPolicyIteration.m
+++ Assignment2/Exercise3/WalkPolicyIteration.m
@@ -1,7 +1,7 @@
 function [ output_args ] = WalkPolicyIteration( s )
 
 load('delta.mat');
-load('reward.mat');
+load('rtest.mat');
 
 % n = # states
 % m = # actions
@@ -13,9 +13,14 @@ policy = ceil(rand(n,1)*4);
 % Discount Factor
 y = 0.999;
 
+% Iteration Counter
+cnt = 0;
+
 policy_old = policy;
 
 while 1
+    % Increase Counter
+    cnt = cnt + 1;
     I = eye(n);
     b = zeros(16,1);
     % For each state
@@ -23,22 +28,38 @@ while 1
         b(j) = rew(j, policy(j));
         I(j, d(j,policy(j))) = -y;
     end
+    % Solve system of equations
     vPi = I\b;
     
     for j = 1:n
         [~,policy(j)] = max(rew(j,:)' + y*vPi(d(j,:)));
     end
     
-    if norm(policy-policy_old)<=0.1
+    if norm(policy-policy_old)<=0.01
         break;
     else
         policy_old = policy;
     end
 end
 
+% state sequence
+q = zeros(1,16);
+% initial state
+q(1) = s;
 
+for i = 2:length(q)
+    % current state
+    current_state = q(i-1);
+    % next state
+    q(i) = d(current_state,policy(current_state));
+end
 
+load('qtest.mat')
+q-qtest
 
+disp(sprintf('Interations needed: %d',cnt));
+% print cartoon
+walkshow(q);
 
 end
 
